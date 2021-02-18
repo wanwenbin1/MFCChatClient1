03de37d554dbbf961c79a76e6635ebf4d877b909
1093438558@qq.com <1093438558@qq.com>
2021-02-18 17:05:23 +0800

    3

--- MFCChatClient/MFCChatClientDlg.cpp
+++ MFCChatClient/MFCChatClientDlg.cpp
@@ -161,5 +161,7 @@ void CMFCChatClientDlg::OnBnClickedConectBtn()
 	// TODO: 在此添加控件通知处理程序代码
 	TRACE(TEXT("[CharClient]Conect Btn"));
 	TRACE(TEXT("[CharClient]Conect Btn"));
+	TRACE(TEXT("[CharClient]Conect Btn"));
+	TRACE(TEXT("[CharClient]Conect Btn"));
 	
 }
